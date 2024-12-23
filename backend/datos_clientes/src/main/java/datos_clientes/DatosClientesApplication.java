a12a65fbd0dcb2e7d766007bc1d1cf5e240f6091
esteban <esteban.pg.87@gmail.com>
2024-12-23 12:49:16 +0000

    backend basico

---
+++ backend/datos_clientes/src/main/java/datos_clientes/DatosClientesApplication.java
@@ -0,0 +1,12 @@
+package datos_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatosClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatosClientesApplication.class, args);
+	}	
+}
---
+++ backend/datos_clientes/src/test/java/datos_clientes/DatosClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package datos_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatosClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
